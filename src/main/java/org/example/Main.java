c464cc8240d315cd2d18c8bbedbbe2b756f39ef3
Speedefix <snorreschulte@gmail.com>
2023-04-22 01:00:54 +0200

    my-strategie

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,14 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
+    static int count_my = 0;
     static int count_bernd = 0;
     static int count_berta = 0;
+
+    static List<Integer> myList = new ArrayList<>();
     static List<Integer> berndList = new ArrayList<>();
     static List<Integer> bertaList = new ArrayList<>();
 
@@ -16,11 +17,12 @@ public class Main {
         int upperBound = 20;
 
         for (int i = lowerBound; i < upperBound; i++) {
-            runBerndAndBerta(i);
+            runStrategies(i);
         }
 
         for (int startingValue = lowerBound; startingValue < upperBound; startingValue++) {
             System.out.println("input: " + startingValue);
+            System.out.println("My count: " + myList.get(startingValue - lowerBound));
             System.out.println("Bernd count: " + berndList.get(startingValue - lowerBound));
             System.out.println("Berta count: " + bertaList.get(startingValue - lowerBound));
 
@@ -28,9 +30,13 @@ public class Main {
         }
     }
 
-    private static void runBerndAndBerta(double startingValue) {
+    private static void runStrategies(double startingValue) {
+        count_my = 0;
         count_bernd = 0;
         count_berta = 0;
+
+        my(startingValue);
+        myList.add(count_my);
 
         bernd(startingValue);
         berndList.add(count_bernd);
@@ -39,6 +45,18 @@ public class Main {
         bertaList.add(count_berta);
     }
 
+
+    private static void my(double b) {
+        if (b == 0) {
+            // System.out.println(1);
+            count_my++;
+        } else {
+            my(b-1);
+            // System.out.println(1);
+            count_my++;
+            my(b-1);
+        }
+    }
     private static void bernd(double b) {
         if (b == 0) {
             // System.out.println(1);
@@ -61,5 +79,4 @@ public class Main {
             berta(b - 1);
         }
     }
-
 }
